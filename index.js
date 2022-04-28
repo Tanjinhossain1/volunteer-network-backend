700d40f1355f7a2f537687dd8e3b0b6e98c59315
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-04-28 10:22:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const { query } = require('express');
+
+require('dotenv').config()
+
+app.use(cors());
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ja3y1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    await client.connect();
+    const volunteerCollection = client.db("leader").collection("volunteers");
+    const newVolunteer = client.db("newVolunteer").collection("volunteer");
+    const createVolunteers = client.db("createVolunteer").collection("volunteer");
+    const addVolunteers = client.db("addVolunteer").collection("volunteer");
+
+    try {
+        app.get('/volunteer', async (req, res) => {
+          
+            const query = {};
+            const cursor = volunteerCollection.find(query);
+            const volunteer = await cursor.toArray();
+            res.send(volunteer)
+        })
+        app.get('/checkVolunteer', async (req, res) => {
+            const query = {};
+            const cursor = createVolunteers.find(query);
+            const volunteer = await cursor.toArray();
+            res.send(volunteer)
+        })
+
+        app.get('/newVolunteer', async (req, res) => {
+            
+            const query = {};
+            const cursor = newVolunteer.find(query);
+            const volunteer = await cursor.toArray();
+            res.send(volunteer)
+        })
+        app.get('/volunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const collectedVolunteer = await volunteerCollection.findOne(query);
+            res.send(collectedVolunteer)
+        })
+
+        app.post('/volunteer', async (req, res) => {
+            const volunteerDetail = req.body;
+            const createVolunteer = await newVolunteer.insertOne(volunteerDetail);
+            res.send(createVolunteer)
+        })
+
+
+        app.post('/volunteerCreate', async (req, res) => {
+            const volunteerDetail = req.body;
+            const createVolunteer = await createVolunteers.insertOne(volunteerDetail);
+            res.send(createVolunteer)
+        })
+
+
+        app.post('/addVolunteer',async(req,res)=>{
+            const addDetail = req.body;
+            console.log(addDetail)
+            const addedVolunteer = await volunteerCollection.insertOne(addDetail);
+            res.send(addedVolunteer)
+        })
+
+
+        app.delete('/checkVolunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteVolunteer = await createVolunteers.deleteOne(query);
+            res.send(deleteVolunteer)
+        })
+
+
+        app.delete('/newVolunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteVolunteer = await newVolunteer.deleteOne(query);
+            res.send(deleteVolunteer)
+        })
+        app.delete('/volunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const deleteVolunteer = await volunteerCollection.deleteOne(query);
+            res.send(deleteVolunteer)
+        })
+    }
+    finally {
+        // client.close();
+    }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
